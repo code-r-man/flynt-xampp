c439b0d779be81180b81bac19f2c281c333d8dc5
Nikola Jovanovic <ing.nikola.jovanovic@gmail.com>
2020-02-10 23:23:25 +0100

    Update install-wordpress.bat

--- install-wordpress.bat
+++ install-wordpress.bat
@@ -1,5 +1,5 @@
 @echo off
 del wp-config.php && ^
-wp config create --dbname=wordpress --dbuser=master --dbpass=flatron && ^
-wp db create --dbuser=master --dbpass=flatron && ^
-wp core install --url=flynt.wordpress --title=Flynt --admin_user=admin --admin_password=admin --admin_email=asasino.nn@gmail.com
+wp config create --dbname=wordpress --dbuser=master --dbpass=passSSAA && ^
+wp db create --dbuser=master --dbpass=passSSAA && ^
+wp core install --url=flynt.wordpress --title=Flynt --admin_user=admin --admin_password=admin --admin_email=email@gmail.com
